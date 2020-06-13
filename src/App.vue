5348833dccd217fe89b58ff59c1007df5ab693d5
Augusto Gehrke <augustogehrke@outlook.com>
2020-06-13 09:12:52 -0300

    Ajuste no layout para login não ficar dentro do v-app

--- src/App.vue
+++ src/App.vue
@@ -1,96 +1,12 @@
 <template>
   <div id="app">
-    <v-app id="inspire">
-      <v-navigation-drawer
-        v-model="drawer"
-        app
-      >
-        <v-list dense>
-          <v-list-item link>
-            <v-list-item-action @click="toMapPage">
-              <v-icon color="green darken-2" large>mdi-google-maps</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title @click="toMapPage">Mapa</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item link>
-            <v-list-item-action @click="toSponsorsPage">
-              <v-icon color="orange darken-2" large>mdi-account-star</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title @click="toSponsorsPage">Patrocinadores</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item link>
-            <v-list-item-action @click="logout">
-              <v-icon color="red darken-2" large>mdi-account-arrow-right-outline</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title @click="logout">Sair</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-
-      <v-app-bar
-        app
-        color="indigo"
-        dark
-      >
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title>Unindo atletas por uma paixão</v-toolbar-title>
-      </v-app-bar>
-
-      <v-content>
-        <v-container
-          class="fill-height"
-          fluid
-        >
-          <v-row
-            align="center"
-            justify="center"
-          >
-            <router-view></router-view>
-          </v-row>
-        </v-container>
-      </v-content>
-      <v-footer
-        color="indigo"
-        app
-      >
-        <span class="white--text">SprintIN &copy; 2020</span>
-      </v-footer>
-    </v-app>
+    <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  props: {
-    source: String
-  },
-  data () {
-    return {
-      drawer: false
-    }
-  },
-  methods: {
-    toSponsorsPage () {
-      this.$router.push({ name: 'sponsors' })
-    },
-    toMapPage () {
-      this.$router.push({ name: 'maps' })
-    },
-    async logout () {
-      this.$firebase.logout()
-      this.$store.commit('setName', null)
-      this.$store.commit('setPhoto', null)
-      this.$store.commit('setToken', null)
-      this.$router.push({ name: 'login' })
-    }
-  }
+  name: 'app'
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import AppLayout from '@/components/layout/AppLayout'
 import Maps from '@/components/maps/Maps'
 import Sponsors from '@/components/sponsors/Sponsors'
 import Login from '@/components/login/Login'
@@ -10,25 +11,32 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'maps',
-      component: Maps,
-      meta: {
-        requireAuth: true
-      }
-    },
-    {
-      path: '/patrocinadores',
-      name: 'sponsors',
-      component: Sponsors,
-      meta: {
-        requireAuth: true
-      }
-    },
-    {
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '',
+      component: AppLayout,
+      children: [
+        {
+          path: '/',
+          default: true,
+          name: 'maps',
+          component: Maps,
+          meta: {
+            requireAuth: true
+          }
+        },
+        {
+          path: 'patrocinadores',
+          name: 'sponsors',
+          component: Sponsors,
+          meta: {
+            requireAuth: true
+          }
+        }
+      ]
     }
   ]
 })
