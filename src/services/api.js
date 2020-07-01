3eb47a477854089f6ec06bcaf7c4498084fe7b8b
Augusto Gehrke <augustogehrke@outlook.com>
2020-07-01 20:30:00 -0300

    Adição de interceptors no axios para pegar o token

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,22 @@
 const axios = require('axios')
 
-const vuex = localStorage.getItem('vuex')
-const { user } = JSON.parse(vuex)
-
 const api = axios.create({
   baseURL: 'http://localhost:3000',
-  timeout: 5000,
-  headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${user.token}` }
+  timeout: 7000,
+  headers: { 'Content-Type': 'application/json' }
 })
 
-module.exports = api
+api.interceptors.request.use(async (config) => {
+  // if (!config.url.endsWith('login')) {}
+  const vuex = await localStorage.getItem('vuex')
+  if (vuex) {
+    const data = JSON.parse(vuex)
+    config.headers.Authorization = `Bearer ${data.user.token}`
+  }
+
+  return config
+}, (error) => {
+  return Promise.reject(error)
+})
+
+export default api
