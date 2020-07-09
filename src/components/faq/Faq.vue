c6709de2830c37282c1b65b96e4b98e415248c72
Augusto Gehrke <augustogehrke@outlook.com>
2020-07-08 21:55:08 -0300

    Atualização do faz

--- src/components/faq/Faq.vue
+++ src/components/faq/Faq.vue
@@ -20,6 +20,21 @@
       </v-expansion-panel-content>
     </v-expansion-panel>
     <v-expansion-panel>
+      <v-expansion-panel-header class="title">Aplicativo ou plataforma</v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <p>As duas opções. O Sport4All foi desenvolvido com uma tecnologia que permite ser utilizado em qualquer dispositivo, seja
+        celular, tablet ou computador e se comportar como uma plataforma ou aplicativo.</p>
+        <p><strong>Plataforma:</strong></p>
+        <p>Você utilizará um navegador para abrir o link (sport4all.com.br) e utilizá normalmente como um site.</p>
+        <p><strong>Aplicativo:</strong></p>
+        <p>Através de um navegador também, na primeira vez que entrar pelo celular aparecerá na parte inferior da tela a opção "Adicionar o app Sport4All
+          à tela inicial". Com isso, o app será baixado/adicionado na sua lista de aplicativos e poderá ser aberto como um
+          aplicativo normal, não mais pelo navegador e ainda sem precisar baixar da loja :).</p>
+        <p>Também é possível fazer esse processo abrindo as configuração do navegador e clicar em "Adicionar a tela inicial"
+        </p>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+    <v-expansion-panel>
       <v-expansion-panel-header class="title">Funcionalidades</v-expansion-panel-header>
       <v-expansion-panel-content>
         <p><strong>Ver eventos</strong></p>
@@ -31,6 +46,29 @@
         pessoas possam fazer a prática esportiva com você. </p>
         <p>OBS: Para criar um novo evento é preciso apenas um duplo clique
         na regição do mapa que deseja.</p>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+    <v-expansion-panel>
+      <v-expansion-panel-header class="title">Quero colaborar</v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <p>O Sport4All é gratuito e sempre será, entretanto, existem custos mensais para manter o app no ar.</p>
+        <p>Existem algumas opções para você colaborar com o projeto:</p>
+        <p>* Compartilhar com seus grupos e amigos a plataforma para mais atletas criarem ou participarem de eventos;</p>
+        <p>* Você empresário pode se tornar um patrocinador oficial deste projeto;</p>
+        <p>* Todo usuário pode colaborar trazendo ideias, doando brindes para futuros sorteios, ajudar no desenvolvimento
+          ou marketing do app, ou realizar sua contribuição financeira.</p>
+        <p>Entre em contato através da conta sport4all do instagram para saber mais informações</p>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+    <v-expansion-panel>
+      <v-expansion-panel-header class="title">Novidades da versão 2.0</v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <p>* Veja quais serão as próximas funcionalidades do app na sua versão 2.0:</p>
+        <p>* Criação da rota do evento no mapa;</p>
+        <p>* Upload de imagens dos trajetos do evento;</p>
+        <p>* Chat do evento para conversa;</p>
+        <p>* Mais filtros para aplicar no mapa.</p>
+        <p><strong> Não deixe de nos informar o que gostaria de ter no app.</strong></p>
       </v-expansion-panel-content>
     </v-expansion-panel>
   </v-expansion-panels>
